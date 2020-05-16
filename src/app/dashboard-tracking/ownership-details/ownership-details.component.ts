816221652988a4459800804927531fb45710777d
Easy-Cloud-in <sakar@easy-cloud.in>
2020-05-16 22:37:28 +0530

    svg marker added

--- src/app/dashboard-tracking/ownership-details/ownership-details.component.ts
+++ src/app/dashboard-tracking/ownership-details/ownership-details.component.ts
@@ -1,23 +1,26 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { FormBuilder, Validators, FormGroupDirective, FormGroup } from '@angular/forms';
-import { ObservableService } from './../../services/observable.service';
-import { IMapLocation } from 'src/app/models/maplocation';
-import { MapServiceService } from 'src/app/services/map/map-service.service';
-import { TosterType, mapType } from "../../enum/enums"
-import * as _ from 'lodash';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import {
+  FormBuilder,
+  Validators,
+  FormGroupDirective,
+  FormGroup,
+} from "@angular/forms";
+import { ObservableService } from "./../../services/observable.service";
+import { IMapLocation } from "src/app/models/maplocation";
+import { MapServiceService } from "src/app/services/map/map-service.service";
+import { TosterType, mapType } from "../../enum/enums";
+import * as _ from "lodash";
 // declare variable
 //  import { * as  L } from '../../asserts/leaflet/leaflet';
 
 declare let L: any;
 
 @Component({
-  selector: 'app-ownership-details',
-  templateUrl: './ownership-details.component.html',
-  styleUrls: ['./ownership-details.component.scss']
+  selector: "app-ownership-details",
+  templateUrl: "./ownership-details.component.html",
+  styleUrls: ["./ownership-details.component.scss"],
 })
 export class OwnershipDetailsComponent implements OnInit {
-
-
   public map: any;
   public marker: any;
 
@@ -33,17 +36,22 @@ export class OwnershipDetailsComponent implements OnInit {
     pincode: "",
     suburb: "",
     lat: "",
-    lng: ""
-  }
+    lng: "",
+  };
 
   mapLocationForm: FormGroup;
 
+  // markerIcon = L.icon({
+  //   iconUrl: "../../assets/leaflet/images/marker-icon.png",
+  //   shadowUrl: "../../assets/leaflet/images/marker-icon.png",
+  // });
+
   markerIcon = L.icon({
-    iconUrl: "../../assets/leaflet/images/marker-icon.png",
-    shadowUrl: "../../assets/leaflet/images/marker-icon.png",
-  })
-
-
+    iconUrl: "../../assets/img/maker-cow-01.svg",
+    iconSize: [100, 100],
+    iconAnchor: [50, 80],
+    // popupAnchor: [100, -100],
+  });
   /*  zoomlevels = [
      { value: '2', viewValue: 'Continent/ sea' },
      { value: '4', viewValue: 'Country' },
@@ -60,22 +68,41 @@ export class OwnershipDetailsComponent implements OnInit {
     private fb: FormBuilder,
     private observableService: ObservableService,
     private mapService: MapServiceService
-  ) {
-
-
-  }
-
-
-
+  ) {}
 
   ngOnInit() {
+    // let osm = L.tileLayer('https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png', {
+    //   attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    // })
 
-    let osm = L.tileLayer('https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png', {
-      attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    })
+    // let url =
+    //   "https://tile.thunderforest.com/cycle/{z}/{x}/{y}.png?apikey=0044ae63e2434d1380cf37327d6677b6";
+
+    // let url =
+    //   "https://tile.thunderforest.com/transport/{z}/{x}/{y}.png?apikey=0044ae63e2434d1380cf37327d6677b6";
+
+    let url =
+      "https://tile.thunderforest.com/neighbourhood/{z}/{x}/{y}.png?apikey=0044ae63e2434d1380cf37327d6677b6";
+
+    // let url =
+    //   "https://tile.thunderforest.com/mobile-atlas/{z}/{x}/{y}.png?apikey=0044ae63e2434d1380cf37327d6677b6";
+
+    // let url =
+    //   "https://tile.thunderforest.com/pioneer/{z}/{x}/{y}.png?apikey=0044ae63e2434d1380cf37327d6677b6";
+
+    // let url =
+    //   "https://tile.thunderforest.com/spinal-map/{z}/{x}/{y}.png?apikey=0044ae63e2434d1380cf37327d6677b6";
+
+    // let url =
+    //   "https://tile.thunderforest.com/outdoors/{z}/{x}/{y}.png?apikey=0044ae63e2434d1380cf37327d6677b6";
+
+    let osm = L.tileLayer(url, {
+      attribution:
+        '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    });
 
     // .addTo(map);
-    this.map = L.map('map')
+    this.map = L.map("map")
       .addLayer(osm)
       .setView([22.998851594142913, 78.44238281249999], 4);
 
@@ -104,19 +131,16 @@ export class OwnershipDetailsComponent implements OnInit {
       suburb: [""],
       pincode: [""],
       lat: [""],
-      lng: [""]
-
+      lng: [""],
     });
 
     // set form values
     this.initMapData();
-
   }
 
   // ==== init map data ====
 
   initFormData(data): void {
-
     this.mapLocationForm.setValue({
       country: data.country,
       countryCode: data.countryCode,
@@ -129,8 +153,8 @@ export class OwnershipDetailsComponent implements OnInit {
       suburb: data.suburb,
       pincode: data.pincode,
       lat: data.lat,
-      lng: data.lng
-    })
+      lng: data.lng,
+    });
   }
 
   initMapData(): void {
@@ -149,13 +173,11 @@ export class OwnershipDetailsComponent implements OnInit {
 
   // ========= search form group ===========
   searchForm = this.fb.group({
-    search: ["", Validators.required]
+    search: ["", Validators.required],
     // searchList: ["", Validators.required]
   });
 
-
   findPlaceOnMap(mapPlace) {
-
     /* Set up params to send to Nominatim */
     var params = {
       // Defaults
@@ -170,17 +192,17 @@ export class OwnershipDetailsComponent implements OnInit {
       L.Util.getParamString(params);
 
     this.mapService.getlocation(url).subscribe(
-      res => {
+      (res) => {
         this.markOnMap(res);
       },
-      err => {
+      (err) => {
         this.observableService.setTosterMsg({
           type: "error",
           title: "Request failed",
-          message: err
-        })
+          message: err,
+        });
       }
-    )
+    );
   }
 
   json(response) {
@@ -188,7 +210,6 @@ export class OwnershipDetailsComponent implements OnInit {
   }
 
   markOnMap(results) {
-
     this.initMapData();
 
     if (results.length !== 0) {
@@ -198,30 +219,30 @@ export class OwnershipDetailsComponent implements OnInit {
       let first = L.latLng(bbox[0], bbox[2]);
       let second = L.latLng(bbox[1], bbox[3]);
 
-      this.locationData.country = mapData.address.country
-      this.locationData.countryCode = _.upperCase(mapData.address.country_code)
+      this.locationData.country = mapData.address.country;
+      this.locationData.countryCode = _.upperCase(mapData.address.country_code);
 
-      this.locationData.state = mapData.address.state
-      this.locationData.district = mapData.address.state_district
+      this.locationData.state = mapData.address.state;
+      this.locationData.district = mapData.address.state_district;
 
       if (mapData.type === mapType.ADMINISTRATIVE) {
-        this.locationData.taluk = mapData.address.county
+        this.locationData.taluk = mapData.address.county;
       }
 
-      this.locationData.lat = mapData.lat
-      this.locationData.lng = mapData.lon
+      this.locationData.lat = mapData.lat;
+      this.locationData.lng = mapData.lon;
 
       if (mapData.type === mapType.VILLAGE) {
-        this.locationData.village = mapData.address.village
-        this.locationData.taluk = mapData.address.county
+        this.locationData.village = mapData.address.village;
+        this.locationData.taluk = mapData.address.county;
       }
 
       if ("postcode" in mapData.address) {
-        this.locationData.pincode = mapData.address.postcode
+        this.locationData.pincode = mapData.address.postcode;
       }
 
       if ("suburb" in mapData.address) {
-        this.locationData.suburb = mapData.address.suburb
+        this.locationData.suburb = mapData.address.suburb;
       }
 
       this.initFormData(this.locationData);
@@ -231,13 +252,12 @@ export class OwnershipDetailsComponent implements OnInit {
 
       this.marker.setLatLng(new L.LatLng(mapData.lat, mapData.lon));
       this.map.panTo(new L.LatLng(mapData.lat, mapData.lon));
-
     } else {
       this.observableService.setTosterMsg({
         type: TosterType.WARNING,
         title: "wrong spelling",
-        message: "Check the spelling before seaching!"
-      })
+        message: "Check the spelling before seaching!",
+      });
     }
   }
 
@@ -246,14 +266,13 @@ export class OwnershipDetailsComponent implements OnInit {
   }
 
   markerDragEnd(e) {
-
     var position = e.target._latlng;
     console.log(position);
 
     this.marker.setLatLng(new L.LatLng(position.lat, position.lng));
     this.map.panTo(new L.LatLng(position.lat, position.lng));
-    this.locationData.lat = position.lat
-    this.locationData.lng = position.lng
+    this.locationData.lat = position.lat;
+    this.locationData.lng = position.lng;
 
     this.initFormData(this.locationData);
   }
@@ -265,17 +284,14 @@ export class OwnershipDetailsComponent implements OnInit {
   // }
 
   mapClicked(e) {
-
     var position = e.latlng;
-    console.log(position)
+    console.log(position);
 
     this.marker.setLatLng(new L.LatLng(position.lat, position.lng));
     this.map.panTo(new L.LatLng(position.lat, position.lng));
-    this.locationData.lat = position.lat
-    this.locationData.lng = position.lng
+    this.locationData.lat = position.lat;
+    this.locationData.lng = position.lng;
 
     this.initFormData(this.locationData);
-
   }
 }
-
