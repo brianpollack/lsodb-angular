028d6a4747fb6a4542f4734a5d5def2ecb3a3b68
Easy-Cloud-in <sakar@easy-cloud.in>
2020-05-18 10:50:27 +0530

    side menu layout updated

--- src/app/dashboard-tracking/tracking.component.ts
+++ src/app/dashboard-tracking/tracking.component.ts
@@ -1,44 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-import { trigger, transition, style, animate } from '@angular/animations';
+import { Component, OnInit } from "@angular/core";
+import { trigger, transition, style, animate } from "@angular/animations";
 @Component({
-  selector: 'app-tracking',
-  templateUrl: './tracking.component.html',
-  styleUrls: ['./tracking.component.scss'],
+  selector: "app-tracking",
+  templateUrl: "./tracking.component.html",
+  styleUrls: ["./tracking.component.scss"],
   animations: [
-    trigger(
-      'inOutAnimation', 
-      [
-        transition(
-          ':enter', 
-          [
-            style({ width: 0 }),
-            animate('0.5s ease-out', 
-                    style({ width: 250 }))
-          ]
-        ),
-        transition(
-          ':leave', 
-          [
-            style({ width: 250 }),
-            animate('0.5s ease-in', 
-                    style({ width: 0 }))
-          ]
-        )
-      ]
-    )
-  ]
+    trigger("inOutAnimation", [
+      transition(":enter", [
+        style({ width: 0 }),
+        animate("0.5s ease-out", style({ width: 250 })),
+      ]),
+      transition(":leave", [
+        style({ width: 250 }),
+        animate("0.5s ease-in", style({ width: 0 })),
+      ]),
+    ]),
+  ],
 })
 export class TrackingComponent implements OnInit {
-
   showMenu: boolean = false;
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   toggleMenu(btn) {
     console.log(btn.checked);
     btn.checked = !btn.checked;
   }
-
 }
